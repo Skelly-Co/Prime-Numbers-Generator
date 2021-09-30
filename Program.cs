ad357ef6a41ba5855bc5bdf293bec1131098d530
schemabuoi <charyszczakg@gmail.com>
2021-09-30 13:03:39 +0200

    Upload Generator

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace PrimeNumbersGenerator {
+	class Program {
+		static void Main(string[] args) {
+			Generator generator = new Generator();
+			Time(() => {
+				List<long> primeNumbers = generator.GetPrimesSequential(1, 1_000_000);
+				Console.WriteLine(primeNumbers.Count);
+			}, "PrimeNumbersSequential");
+
+			Time(() => {
+				List<long> primeNumbers = generator.GetPrimesSequential(1, 1_000_000);
+				Console.WriteLine(primeNumbers.Count);
+			}, "PrimeNumbersParallel");
+		}
+
+		static void Time(Action action, String taskName) {
+			Stopwatch watch = Stopwatch.StartNew();
+			action.Invoke();
+			watch.Stop();
+			Console.WriteLine(taskName + " - Time elapsed: " + watch.ElapsedMilliseconds);
+		}
+	}
+}
